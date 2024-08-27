34e09c21957c045bcf789665f6a071198c19cb88
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-08-27 18:54:02 +0530

    Created userModel and userAccountModel

---
+++ Models/UserAccountModel.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+const userAccountSchema = new mongoose.Schema({
+    userAccountId: {
+        type: String,
+        default: uuidv4,
+        unique: true,
+        required: true
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    accountType: {
+        type: String,
+        enum: ["GMAIL", "OUTLOOK"],
+        required: true
+    },
+    accountEmail: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    accessToken: {
+        type: String,
+        required: false
+    },
+    refreshToken: {
+        type: String,
+        required: true
+    },
+    expiresAt: {
+        type: Date,
+        required: false
+    }
+}, {
+    timestamps: true
+});
+
+const userAccountModel = new mongoose.model("userAccounts", userAccountSchema);
+module.exports = userAccountModel;
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://abhishekshah5486:i8SBTBdZyySqPNdG@cluster0.szhpw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+const app = express();
+require('dotenv').config();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log("Connected to MongoDB");
 })
@@ -8,7 +13,24 @@ mongoose.connect("mongodb+srv://abhishekshah5486:i8SBTBdZyySqPNdG@cluster0.szhpw
     console.log(err);
 });
 
-const app = express();
+const productSchema = new mongoose.Schema({
+    product_name: {
+        type: String,
+        required: true
+    }
+});
+
+const productModel = new mongoose.model("Product", productSchema);
+// Create a product
+app.post('/product', async (req, res) => {
+    const product = productModel.create({
+        product_name: req.body.product_name
+    })
+    console.log(product);
+    return res.status(201).json({
+        message: "Product created successfully"
+    });
+});
 
 const port = 8081 || process.env.PORT;
 app.listen(port, () => {
