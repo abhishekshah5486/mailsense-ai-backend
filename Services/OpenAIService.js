db3600199dc235713c1b3037f56e24bc59e6ccdd
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-08-29 15:08:37 +0530

    Made minor fixes in OpenAIService.js file

--- Services/OpenAIService.js
+++ Services/OpenAIService.js
@@ -54,30 +54,18 @@ Sample 2:
 
 const thread = [
     {
-        "Message ID": "19195a359eccac46",
-        "Subject": "Hhh",
-        "From": "Naresh Biradar <nareshdb555@gmail.com>",
-        "To": "biradar6438@gmail.com",
-        "Date": "Wed, 28 Aug 2024 02:29:56 +0530",
-        "Is Unread": "No",
-        "Content Preview": "birada"
-    },
-    {
-        "Message ID": "1919a99a4efb156d",
-        "Subject": "Re: Hhh",
-        "From": "Naresh Biradar <biradar6438@gmail.com>",
-        "To": "Naresh Biradar <nareshdb555@gmail.com>",
-        "Date": "Thu, 29 Aug 2024 01:37:14 +0530",
-        "Is Unread": "Yes",
-        "Content Preview": "Replied to db555"
+      "id": "1916cb3fde9851ca",
+      "from": "Codeforces@codeforces.com",
+      "to": "nareshdb555@gmail.com",
+      "subject": "Codeforces Round 967 (Div. 2)",
+      "date": "Mon, 19 Aug 2024 23:30:52 +0300 (MSK)",
+      "body": "Hello, naresh50. Welcome to the regular Codeforces round.\r\n\r\nI'm glad to invite you to take part in Codeforces Round 967 (Div. 2). It starts on Tuesday, August, 20, 2024 14:35 (UTC). The contest duration is 2 hours. The allowed programming languages are C/C++, Pascal, Perl, Java, C#, Python (2 and 3), Ruby, PHP, Haskell, Scala, OCaml, D, Go, JavaScript and Kotlin.\r\n\r\nThe round writer is Misuki. Do not miss the round!\r\n\r\nThe round will be held on the rules of Codeforces, so read the rules (here and here) beforehand.\r\n\r\nIt will be for newcomers or participants from the second division (non-rated users or those having less than 2100 rating points). Want to compete? Do not forget to register for the contest and check your handle on the registrants page. The registration will be closed 5 minutes before the contest.\r\n\r\nRegister Now →  If you have any questions, please feel free to ask me on the pages of Codeforces. If you no longer wish to receive these emails, click https://codeforces.com/unsubscribe/contests/d7c5a10720bb43a356a579e325a9d1aabd7ebf4b/ to unsubscribe.\r\n\r\nWish you high rating, MikeMirzayanov and Codeforces team"
     }
-];
-
-const email = "biradar6438@gmail.com";
+  ]
 
 // Format the thread of messages and generate a response
-const formattedEmailThread = ({email, thread}) => {
-    const userEmail = email;
+const formattedEmailThread = (thread) => {
+    const userEmail = thread[thread.length - 1]["to"];
     const formattedThread = thread.map((threadMessage) => {
         const role = threadMessage["From"].includes(userEmail) ? 'assistant' : 'user';
         return {
@@ -111,5 +99,6 @@ generateResponse({email, thread}).then((res) => {
 module.exports = {
     generateResponse,
 }
+
 
 
